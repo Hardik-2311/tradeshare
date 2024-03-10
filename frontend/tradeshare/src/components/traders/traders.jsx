127ac6fc56723fc6bbd23e243a18fc821a5641cc
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2024-03-10 23:13:38 +0530

    done

--- frontend/tradeshare/src/components/traders/traders.jsx
+++ frontend/tradeshare/src/components/traders/traders.jsx
@@ -5,7 +5,7 @@ import Layout from "../Layout";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 const Traders = () => {
-    const navigate=useNavigate();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const traders = useSelector((state) => state.traders.traders);
   const [selectedTrader, setSelectedTrader] = useState(null);
@@ -15,31 +15,33 @@ const Traders = () => {
   }, [dispatch]);
 
   const handleTraderClick = (trader) => {
-    navigate("/tdash")
+    navigate("/tdash");
   };
   const currentUserData = localStorage.getItem("currentUser");
   const currentUser = JSON.parse(currentUserData);
-  const handlesubmission = async (traderId, clientId)=>{
-    const data={
-        "client":clientId,
-        "trader":traderId,
-        "start_date":"2022-03-11",
-        "end_date":"2022-04-11",
-        "price":500,
-    }
+  const handlesubmission = async (traderId, clientId) => {
+    const data = {
+      client: clientId,
+      trader: traderId,
+      start_date: "2022-03-11",
+      end_date: "2022-04-11",
+      price: 500,
+    };
 
-    const response =await axios.post("http://127.0.0.1:8000/subscription/",data);
-    console.log(response)
-  }
+    const response = await axios.post(
+      "http://127.0.0.1:8000/subscription/",
+      data
+    );
+    console.log(response);
+  };
   return (
     <div className="container mx-auto overflow-y-scroll h-[80vh]">
       <h1 className="text-3xl font-bold my-4">Traders</h1>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="flex flex-col gap-4">
         {traders.map((trader, index) => (
           <div
             key={index}
-            className="bg-gray-200 p-4 rounded shadow"
-            // onClick={() => handleTraderClick(trader)}
+            className="bg-[#202020] p-4 text-white rounded shadow flex justify-evenly items-center w-[90%] mx-auto"
           >
             <h2 className="text-xl font-semibold">{trader.username}</h2>
             <h2 className="text-xl font-semibold">{trader.id}</h2>
@@ -47,7 +49,12 @@ const Traders = () => {
             <p>Total Gain: ${trader.total_gain}</p>
             <p>Average Return: {trader.average_return}%</p>
             <p>Trades: {trader.total_trades}</p>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>{handlesubmission(trader.id,currentUser.id)}}>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                handlesubmission(trader.id, currentUser.id);
+              }}
+            >
               premium
             </button>
           </div>
--- frontend/tradeshare/src/components/trades/Trade.jsx
+++ frontend/tradeshare/src/components/trades/Trade.jsx
@@ -3,14 +3,16 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchTrades } from "../../features/tradeSlice";
 import axios from "axios";
 const Trades = () => {
-    const [subscriptions, setSubscriptions] = useState([]);
+  const [subscriptions, setSubscriptions] = useState([]);
   const dispatch = useDispatch();
   const trades = useSelector((state) => state.trades.trades);
+  console.log(trades);
   const loading = useSelector((state) => state.trades.loading);
   const error = useSelector((state) => state.trades.error);
   const currentUserData = localStorage.getItem("currentUser");
   const currentUser = JSON.parse(currentUserData);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantity, setTotalQuantity] = useState(0);
   const userTrades = trades.filter((trade) => trade.user === currentUser.id);
   useEffect(() => {
     dispatch(fetchTrades());
@@ -18,25 +20,26 @@ const Trades = () => {
   useEffect(() => {
     const fetchSubscriptions = async () => {
       try {
-        const response = await axios.get(
-          `http://localhost:8000/subscription/`
-        );
+        const response = await axios.get(`http://localhost:8000/subscription/`);
         setSubscriptions(response.data);
-        console.log(response)
-       ;
+        console.log(response);
       } catch (error) {
-       console.log(error)
+        console.log(error);
       }
     };
 
     fetchSubscriptions();
   }, [currentUser.id]);
   useEffect(() => {
-    let totalPrice = 0;
+    let Price = 0;
+    let totalStocks = 0;
     userTrades.forEach((trade) => {
-      totalPrice += trade.quantity * trade.price;
+      Price += trade.quantity * trade.price;
+      totalStocks += trade.quantity;
     });
-    setTotalPrice(totalPrice);
+    setTotalQuantity(totalStocks);
+    setTotalPrice(Price);
+    console.log(totalPrice, totalQuantity);
   }, [userTrades]);
 
   if (loading) {
@@ -49,9 +52,9 @@ const Trades = () => {
 
   return (
     <div className="overflow-y-scroll h-[80vh] w-[90%] mt-4 flex gap-4 no-scrollbar mx-auto">
-      <div className="flex flex-col gap-4 w-[50%]">
-        <div className="bg-gray-700 p-4 flex gap-4 justify-evenly text-white">
-          <div>Stock Symbol</div>
+      <div className="flex flex-col w-[50%]">
+        <div className="bg-[#202020] p-8 flex gap-2 justify-evenly text-white">
+          <div>Stock</div>
           <div>Quantity</div>
           <div>Price</div>
           <div>Date</div>
@@ -60,7 +63,7 @@ const Trades = () => {
         {userTrades.map((trade) => (
           <div
             key={trade.id}
-            className="bg-[#202020] p-8 shadow  flex gap-4 justify-evenly"
+            className="bg-[#202020] p-8 shadow  border border-b-white flex gap-4 justify-evenly"
           >
             <h3 className="text-lg text-[#5190D8]">{trade.stock_name}</h3>
             <p className="text-white">{trade.quantity}</p>
@@ -76,8 +79,8 @@ const Trades = () => {
           </div>
         ))}
       </div>
-      <div className="flex flex-col gap-4 w-[50%]">
-        <div className="bg-gray-700 p-4 flex gap-4 justify-evenly text-white">
+      <div className="flex flex-col w-[50%]">
+        <div className="bg-[#202020] p-8 flex gap-4 justify-evenly text-white">
           <div>Subscription ID</div>
           <div>Trader ID</div>
           <div>End Date</div>
@@ -86,7 +89,7 @@ const Trades = () => {
         {subscriptions.map((subscription) => (
           <div
             key={subscription.id}
-            className="bg-[#202020] p-8 shadow  flex gap-4 justify-evenly"
+            className="bg-[#202020] p-8 border border-white shadow  flex gap-4 justify-evenly"
           >
             <p className="text-white">{subscription.id}</p>
             <p className="text-white">{subscription.trader}</p>
@@ -94,6 +97,15 @@ const Trades = () => {
             <p className="text-white">{subscription.price}</p>
           </div>
         ))}
+        <div className="bg-[#5190D8] p-8 mt-4 flex gap-4 justify-evenly text-white">
+          <div className="text-white">
+            Total Price: ${totalPrice.toFixed(2)}
+          </div>
+          <div className="text-white">Total Quantity: {totalQuantity}</div>
+          <div>
+            <button>Analyze</button>
+          </div>
+        </div>
       </div>
     </div>
   );
